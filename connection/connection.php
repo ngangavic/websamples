abc52e8047be7fec201f20414284e8eb82afe6e8
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-08-27 15:47:09 +0300

    Update connection.php

--- connection/connection.php
+++ connection/connection.php
@@ -4,14 +4,13 @@ $username="root";
 $password="";
 $host="localhost";
 
+//mysqli connection 
 $conn=mysqli_connect($host,$username,$password,$database);
 
 if(!$conn){
  echo 'Connection error!!!';
 }
 
-
-//user   id10471848_root
-//name  id10471848_websample
-//host localhost
+//PDO connection for use in AJAX
+$pdo = new PDO("mysql:host=localhost;dbname=websample", "root", "");
 ?>
