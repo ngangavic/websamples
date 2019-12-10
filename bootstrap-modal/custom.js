554c1efc88f34a3ac1237160c06606e58d339aaf
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-12-10 21:45:46 +0300

    bootstrap prevent from closing

--- bootstrap-modal/custom.js
+++ bootstrap-modal/custom.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $("#modal1").click(function(){
-        $('#myModal1').modal('hide')
-        $('#myModal2').modal('show')
+        $('#myModal1').modal('hide');
+        $('#myModal2').modal('show');
     });
 });
 
 $(document).ready(function(){
     $("#modal2").click(function(){
-        $('#myModal1').modal('show')
-        $('#myModal2').modal('hide')
+        $('#myModal1').modal('show');
+        $('#myModal2').modal('hide');
     });
 });
