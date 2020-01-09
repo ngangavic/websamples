62e1b54a3360556c65ea34c8426169ac06e0646f
Ng'ang'a Victor <ngangavictor10@gmail.com>
2020-01-09 18:47:34 +0300

    login password verify

--- login/login.php
+++ login/login.php
@@ -3,35 +3,44 @@
 require "../connection/connection.php";
 
 //get data from form and assign it to variables
-$email=$_POST['email'];
-$password=$_POST['password'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 //ensure the button is clicked
-if(isset($_POST['login'])){
-    
+if (isset($_POST['login'])) {
+
     //ensure the variables have data
-    if(isset($email)&&isset($password)){
+    if (isset($email) && isset($password)) {
         //execute login
-        $stmt=$conn->prepare("SELECT * FROM tbl_register WHERE email=? AND password=?");
-        $stmt->bind_param("ss",$email,$password);
-        $stmt->execute();
-        $result=$stmt->get_result();
-        $row=$result->num_rows;
-        if($row>0){
-            //login successful
-            header("location:index.php?info=success");
-        }else{
-            //login error
-            header("location:index.php?info=error");
-        }
-
-    }else{
-        //return error
+        verifyUser($conn, $email, $password);
     }
-
-
 }
 
+function verifyUser($conn, $email, $password)
+{
+    $stmt = $conn->prepare("SELECT * FROM tbl_register WHERE email=?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $count = $result->num_rows;
+    if ($count > 0) {
+        //check password
+//        while($row = $result = fetch_array()){
+        $row = $result->fetch_array();
+        $dbPassword = $row['password'];
+        if (password_verify($password, $dbPassword)) {
+            //success login
+            header("location:index.php?info=success");
+        } else {
+            //password error
+            header("location:index.php?info=error");
+        }
+//        }
+    } else {
+        //error
+        header("location:index.php?info=error");
+    }
+}
 
 
 ?>
