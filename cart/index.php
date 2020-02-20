1011f3bd1733d9a704461e868c84c562bd8c9076
ngangavic <ngangavictor10@gmail.com>
2020-02-20 14:11:38 +0300

    display item in cart

--- cart/index.php
+++ cart/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 //database connection
-$link=mysqli_connect("localhost","root","","websamples")
+$link = mysqli_connect("localhost", "root", "", "websamples")
 
 ?>
 <!DOCTYPE html>
@@ -22,29 +22,29 @@ $link=mysqli_connect("localhost","root","","websamples")
 <body>
 
 <?php
-if(isset($_POST['id'])){
+if (isset($_POST['id'])) {
     $pid = $_POST['id'];
-    $wasFound =false;
+    $wasFound = false;
     $i = 0;
     //if the cart session variable is not set or cart array is empty
-    if(!isset($_SESSION["cart"])||count($_SESSION["cart"])<1){
+    if (!isset($_SESSION["cart"]) || count($_SESSION["cart"]) < 1) {
         //Run if the cart is empty or not set
-        $_SESSION["cart"]=array(1=>array("item_id"=>$pid, "quantity" =>1));
-    }else{
+        $_SESSION["cart"] = array(1 => array("item_id" => $pid, "quantity" => 1));
+    } else {
         //Run if the cart has atleast one item in it
-        foreach($_SESSION["cart"]as $each_item){
+        foreach ($_SESSION["cart"] as $each_item) {
             $i++;
-            while(list($key, $value)=each($each_item)){
-                if($key=="item_id" && $value==$pid){
+            while (list($key, $value) = each($each_item)) {
+                if ($key == "item_id" && $value == $pid) {
                     //That item ps in cart already so lets adjust its quantity using array_splice()
-                    array_splice($_SESSION["pos"], $i-1,1, array(array("item_id"=>$pid, "quantity"=>$each_item['quantity']+1)));
-                    $wasFound=true;
+                    array_splice($_SESSION["pos"], $i - 1, 1, array(array("item_id" => $pid, "quantity" => $each_item['quantity'] + 1)));
+                    $wasFound = true;
                 }//close if condition
             }//close while loop
 
         }//close foreach loop
-        if($wasFound==false){
-            array_push($_SESSION["cart"], array("item_id" => $pid, "quantity"=>1));
+        if ($wasFound == false) {
+            array_push($_SESSION["cart"], array("item_id" => $pid, "quantity" => 1));
         }
     }
     header("location: index.php");
@@ -53,31 +53,86 @@ if(isset($_POST['id'])){
 
 
 //clear cart
-if(isset($_GET['cmd'])&& $_GET['cmd']=="emptycart"){
+if (isset($_GET['cmd']) && $_GET['cmd'] == "emptycart") {
     unset($_SESSION["cart"]);
 }
 
 //remove item from cart
-if(isset($_POST['index_to_remove']) && $_POST['index_to_remove']!=""){
+if (isset($_POST['index_to_remove']) && $_POST['index_to_remove'] != "") {
     //access the array and run code to remove that array index
     $key_to_remove = $_POST['index_to_remove'];
-    if(count($_SESSION["cart"])<=1){
+    if (count($_SESSION["cart"]) <= 1) {
         unset($_SESSION["cart"]);
 
-    }else{
+    } else {
         unset($_SESSION["cart"]["$key_to_remove"]);
         sort($_SESSION["cart"]);
     }
 }
 
 //render cart
-$cartOutput="";
-$cartTotal=0;
+$cartOutput = "";
+$cartTotal = 0;
 $totalCost = 0;
 $discount = 0;
-$x='';
-$customer ='';
-$pp_checkout_btn="";
+$x = '';
+$customer = '';
+$pp_checkout_btn = "";
+
+if (!isset($_SESSION["cart"]) || count($_SESSION["cart"]) < 1) {
+    $cartOutput = "<h2 align='center'>The shopping cart is empty</h2>";
+} else {
+    $i = 0;
+    foreach ($_SESSION["cart"] as $each_item) {
+        //$i++;
+        $item_id = $each_item['item_id'];
+        $sql = mysqli_query($link, "SELECT * FROM cart WHERE id='$item_id' ");
+        $count = mysqli_num_rows($sql);
+        if ($count > 0) {
+            $sql = mysqli_query($link, "SELECT * FROM cart WHERE id='$item_id' ");
+            while ($row = mysqli_fetch_array($sql)) {
+                $product_id = $row["id"];
+                $product_name = $row["name"];
+                $price = $row["price"];
+            }
+
+            $pricetotal = $price * $each_item['quantity'];
+            $cartTotal = $pricetotal + $cartTotal;
+            $x = $i + 1;
+            $pp_checkout_btn .= '<input type="hidden" name="item_name_' . $x . '" value="' . $product_name . '">
+								<input type="hidden" name="amount_' . $x . '" value="' . $price . '">
+								<input type="hidden" name="quantity_' . $x . '" value="' . $each_item['quantity'] . '">';
+
+            //table
+            $cartOutput .= "<tr>";
+            $cartOutput .= '<td>' . $product_id . '</td>';
+            $cartOutput .= '<td>' . $product_name . '</td>';
+            $cartOutput .= '<td><form action="" method="post">
+			<input name="quantity" type="text" value="' . $each_item['quantity'] . '" size="1" maxlength="2"/>
+			<input class="btn btn-primary" type="submit" value="change" name="adjustBtn' . $item_id . '" />
+			<input type="hidden" name="item_to_adjust" value="' . $item_id . '" />
+			</form></td>';
+            $cartOutput .= '<td>KES ' . $price . '</td>';
+//            $cartOutput .= '<td>KES ' . number_format($pricetotal, 0) . '</td>';
+            $cartOutput .= '<td><form action="" method="post">
+			<input class="btn btn-danger" type="submit" value="X" name="deleteBtn' . $item_id . '" />
+			<input type="hidden" name="index_to_remove" value="' . $i . '" />
+			</form></td>';
+            $cartOutput .= '</tr>';
+            $i++;
+        } else {
+
+            $cartOutput = "<h2 align='center'>The Item is locked</h2>";
+            header("location: index.php?cmd=emptycart");
+            ?>
+            <script>
+                alert("The Item is locked");
+            </script>
+            <?php
+
+        }
+    }
+}
 
 
 ?>
@@ -97,20 +152,20 @@ $pp_checkout_btn="";
                 </div>
                 <div class="card-footer">
                     <form method="post" action="">
-                        <input type="hidden" class="form-control form-control-sm" value="1" name="id" >
+                        <input type="hidden" class="form-control form-control-sm" value="1" name="id">
                         <p>Kids Laptop<br/>
                             <b>Ksh. 10000</b>
                         </p>
                         <div class="row">
                             <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-                        <div class="form-group">
-                            <input type="number" class="form-control form-control-sm" value="1" name="qty" >
-                        </div>
+                                <div class="form-group">
+                                    <input type="number" class="form-control form-control-sm" value="1" name="qty">
+                                </div>
                             </div>
                             <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-sm btn-success" name="add">Add to Cart</button>
-                        </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-sm btn-success" name="add">Add to Cart</button>
+                                </div>
                             </div>
                         </div>
                     </form>
@@ -125,14 +180,14 @@ $pp_checkout_btn="";
                 </div>
                 <div class="card-footer">
                     <form method="post" action="">
-                        <input type="hidden" class="form-control form-control-sm" value="2" name="id" >
+                        <input type="hidden" class="form-control form-control-sm" value="2" name="id">
                         <p>Ear Phones<br/>
                             <b>Ksh. 500</b>
                         </p>
                         <div class="row">
                             <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
                                 <div class="form-group">
-                                    <input type="number" class="form-control form-control-sm" value="1" name="qty" >
+                                    <input type="number" class="form-control form-control-sm" value="1" name="qty">
                                 </div>
                             </div>
                             <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
@@ -153,14 +208,14 @@ $pp_checkout_btn="";
                 </div>
                 <div class="card-footer">
                     <form method="post" action="">
-                        <input type="hidden" class="form-control form-control-sm" value="3" name="id" >
+                        <input type="hidden" class="form-control form-control-sm" value="3" name="id">
                         <p>Phone<br/>
                             <b>Ksh. 15000</b>
                         </p>
                         <div class="row">
                             <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
                                 <div class="form-group">
-                                    <input type="number" class="form-control form-control-sm" value="1" name="qty" >
+                                    <input type="number" class="form-control form-control-sm" value="1" name="qty">
                                 </div>
                             </div>
                             <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
@@ -177,18 +232,19 @@ $pp_checkout_btn="";
         <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
             <div class="card">
                 <div class="card-body">
-                    <img src="images/6tb-internal-hdd-deprime-kenya-nairobi.jpg" class="card-img" width="150" height="150">
+                    <img src="images/6tb-internal-hdd-deprime-kenya-nairobi.jpg" class="card-img" width="150"
+                         height="150">
                 </div>
                 <div class="card-footer">
                     <form method="post" action="">
-                        <input type="hidden" class="form-control form-control-sm" value="4" name="id" >
+                        <input type="hidden" class="form-control form-control-sm" value="4" name="id">
                         <p>6TB Internal Drive<br/>
                             <b>Ksh. 10000</b>
                         </p>
                         <div class="row">
                             <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
                                 <div class="form-group">
-                                    <input type="number" class="form-control form-control-sm" value="1" name="qty" >
+                                    <input type="number" class="form-control form-control-sm" value="1" name="qty">
                                 </div>
                             </div>
                             <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
@@ -209,6 +265,7 @@ $pp_checkout_btn="";
         <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
             <table class="table">
                 <thead>
+                <th>#</th>
                 <th>Name</th>
                 <th>Quantity</th>
                 <th>Amount</th>
@@ -217,30 +274,15 @@ $pp_checkout_btn="";
                 <tbody>
 
                 <tr>
-                <td>Laptop</td>
-                <td>5 Pieces</td>
-                <td>Ksh. 50000</td>
-                <td>
-                    <a href="#" class="btn btn-sm btn-danger">Remove</a>
-                </td>
+                    <?php echo $cartOutput; ?>
                 </tr>
-
                 <tr>
-                    <td>Laptop</td>
-                    <td>5 Pieces</td>
-                    <td>Ksh. 50000</td>
-                    <td>
-                        <a href="#" class="btn btn-sm btn-danger">Remove</a>
-                    </td>
-                </tr>
+                    <td colspan="0"></td>
+                    <td><b>Total:</b> Ksh. <?php number_format($pricetotal, 0); ?>
 
-                <tr>
-                    <td colspan="2" ></td>
-                    <td><b>Total:</b> Ksh. 50000</td>
-                    <td>
                         <div class="btn-group">
-                        <a href="#" class="btn btn-sm btn-group-sm btn-success">CheckOut</a>
-                        <a href="index.php?cmd=emptycart" class="btn btn-sm btn-group-sm btn-warning">Clear</a>
+                            <a href="#" class="btn btn-sm btn-group-sm btn-success">CheckOut</a>
+                            <a href="index.php?cmd=emptycart" class="btn btn-sm btn-group-sm btn-warning">Clear</a>
                         </div>
                     </td>
                 </tr>
