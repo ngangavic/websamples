423d313deddbf093e2da25276c791cd56b0c5209
ngangavic <ngangavictor10@gmail.com>
2020-04-18 17:48:30 +0300

    database sql file update

--- connection/websample (2).sql
+++ connection/websample (2).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2019 at 02:47 PM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.6
+-- Generation Time: Apr 18, 2020 at 04:48 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -48,6 +48,33 @@ CREATE TABLE `tbl_register` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_students`
+--
+
+CREATE TABLE `tbl_students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `class` varchar(10) NOT NULL,
+  `age` varchar(5) NOT NULL,
+  `home_town` varchar(100) NOT NULL,
+  `parent_no` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_students`
+--
+
+INSERT INTO `tbl_students` (`id`, `name`, `class`, `age`, `home_town`, `parent_no`) VALUES
+(1, 'John Doe', 'Form 1', '14', 'Nairobi', '+254 722350028'),
+(2, 'Mark Jong', 'Form 1', '14', 'Kisumu', '+254 735282200'),
+(3, 'Mark Jong', 'Form 1', '14', 'Kisumu', '+254 735282200'),
+(4, 'John Mark', 'Form 1', '14', 'Nyeri', '+254 722003528'),
+(5, 'Mark Yan', 'Form 1', '14', 'Nanyuki', '+254 789352822'),
+(6, 'Yuan Yun', 'Form 1', '14', 'Laikipia', '+254 770352822');
+
 --
 -- Indexes for dumped tables
 --
@@ -65,6 +92,12 @@ ALTER TABLE `tbl_register`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_students`
+--
+ALTER TABLE `tbl_students`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -79,6 +112,12 @@ ALTER TABLE `tbl_ajax`
 --
 ALTER TABLE `tbl_register`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_students`
+--
+ALTER TABLE `tbl_students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
